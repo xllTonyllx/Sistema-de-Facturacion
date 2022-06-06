fa1a17b8303cd1a9f910b737a330da4473276379
xllTonyllx <tony.jo.h@uni.pe>
2022-06-05 19:47:04 -0500

    Creando el proyecto

---
+++ src/Package/Factura.java
@@ -0,0 +1,29 @@
+package Package;
+
+import javax.swing.*;
+
+public class Factura {
+    private JButton calcularButton;
+    private JTextField Cant1;
+    private JTextField Cant2;
+    private JTextField Prod1;
+    private JTextField P_Unit2;
+    private JTextField calculoIGV;
+    private JTextField P_Unit1;
+    private JTextField Prod2;
+    private JTextField Subtotal1;
+    private JTextField Subtotal2;
+    private JTextField calculoTotal;
+    private JLabel Cantidad;
+    private JLabel Producto;
+    private JLabel Precio_Unitario;
+    private JLabel Subtotal;
+    private JLabel IGV;
+    private JLabel Total;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+    
+}
